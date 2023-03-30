32ce9e969df53e984ace6cccf95b4f136f12ca84
hunweitseng <h1tseng@cs.ucsd.edu>
2023-03-30 13:49:00 -0700

    CS203 Welcome Assignment First Commit

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{	
+	if (argc > 1) {
+		std::cout << "Goodbye! Winter!\n";
+	} else {
+		std::cout << "Hello! CS203!\n";
+	}
+	return 0;  
+}
